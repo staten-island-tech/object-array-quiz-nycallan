84973a2c8973087d11755489c9c52a85ca03aa6b
nycallan <chernomorets3@gmail.com>
2021-03-04 20:14:34 -0500

    not sure about 4

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const presidents = [
   { name: "Richard Nixon", terms: 2, party: "R" },
   { name: "John F. Kennedy", terms: 1, party: "D" },
   { name: "Lyndon Johnson", terms: 2, party: "D" },
-  { name: "Dwight Eisenhower", terms: 2, party: "R" }
+  { name: "Dwight Eisenhower", terms: 2, party: "R" },
 ];
 
 //Challenge Questions
@@ -23,3 +23,36 @@ const presidents = [
 //4) log all dems who served 2 terms. HINT use chain filter, filter and slice
 
 //BONUS write a script to check if LBJ was a 2 term president. IF he was then alert("LBJ served two terms") else alert "LBJ was one and done"
+
+//1)
+const dems = presidents.filter(function (president) {
+  if (president.party == "D") {
+    return true;
+  }
+});
+console.log(dems);
+
+//2)
+
+const reps = presidents.filter((president) => {
+  if (president.party == "R" && president.terms == 1) {
+    return true;
+  }
+});
+
+console.log(reps);
+
+//3)
+
+const last3 = presidents.slice(0, 3);
+console.log(last3);
+
+//4)
+
+const dem2 = democrat.filter(function (democrat) {
+  if (democrat.terms == 2) {
+    return true;
+  }
+  presidents.slice(0, 11);
+});
+console.log(dem2);
